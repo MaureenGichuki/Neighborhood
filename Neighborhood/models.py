52a5f12be7da73af15a4b5feae02c0e446c23dc3
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-06-21 07:38:42 +0300

    "Feat: This adds the location model"

--- Neighborhood/models.py
+++ Neighborhood/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
+
+# Location 
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # save location
+    def save_location(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
+
