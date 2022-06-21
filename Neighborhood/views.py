d0ca5e39501ef3f27b0c696135e58ee75f544b7e
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-06-21 07:56:42 +0300

    "Feat: adds the view to the homepage"

--- Neighborhood/views.py
+++ Neighborhood/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render,redirect
+from .models import  NeighbourHood, Profile, Post, Business
+from django.contrib.auth.decorators import login_required
+from.forms import NeighbourhoodForm, UpdateProfileForm,PostForm,BusinessForm
 
 # Create your views here.
+def welcome(request):
+    return render(request,'welcome.html')
