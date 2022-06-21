a07f8c9e2fa58e978922c8023272c2a3df08b942
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-06-21 07:41:10 +0300

    "Feat: Adds the neighbourhood model"

--- Neighborhood/models.py
+++ Neighborhood/models.py
@@ -18,3 +18,36 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+# Neighbourhood Model
+
+class NeighbourHood(models.Model):
+
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    occupants_count = models.IntegerField(default=0)
+    police_count = models.IntegerField(default=0)
+    hospital_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def create_neigborhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood(cls, id):
+        cls.objects.filter(id=id).delete()
+
+    
+    @classmethod
+    def search_by_name(cls, search_term):
+        hood = cls.objects.filter(name__icontains=search_term)
+        return hood
+
+    #Finding the neighbourhoods by id
+    @classmethod
+    def find_neigborhood(cls, id):
+        hood = cls.objects.get(id=id)
+        return hood
+    
+    def __str__(self):
+        return "%s neighborhood" % self.name
