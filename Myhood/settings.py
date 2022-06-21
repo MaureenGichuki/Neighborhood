4e68f4549ccdede697525ccdc7508429c559d64d
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-06-21 07:32:09 +0300

    "Feat: Configures the database"

--- Myhood/settings.py
+++ Myhood/settings.py
@@ -74,9 +74,11 @@ WSGI_APPLICATION = 'Myhood.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+  'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myhood',
+        'USER': 'moringa',
+        'PASSWORD': 'myhood',
     }
 }
 
