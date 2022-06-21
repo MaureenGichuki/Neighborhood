757e2d46d503b79e83d407fc9c72959f03029934
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-06-21 08:37:06 +0300

    "Feat: Registers models on admin.py"

--- Neighborhood/admin.py
+++ Neighborhood/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import  NeighbourHood, Profile, Post, Business, Location
+
 
 # Register your models here.
+admin.site.register(NeighbourHood)
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Business)
+admin.site.register(Location)
