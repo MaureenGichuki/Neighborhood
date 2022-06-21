f317b6e842320c45f3aac09c687e01e12c1266d4
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-06-21 07:53:46 +0300

    "Feat: Adds the post form"

--- Neighborhood/forms.py
+++ Neighborhood/forms.py
@@ -10,3 +10,8 @@ class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'content','image')
