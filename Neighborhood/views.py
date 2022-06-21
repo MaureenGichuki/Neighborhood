84ccf9f83cdaf93987c5da5adbeeae8a746be247
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-06-21 07:57:37 +0300

    "Feat:Adds view to featured neighborhoods"

--- Neighborhood/views.py
+++ Neighborhood/views.py
@@ -7,3 +7,18 @@ from.forms import NeighbourhoodForm, UpdateProfileForm,PostForm,BusinessForm
 # Create your views here.
 def welcome(request):
     return render(request,'welcome.html')
+
+@login_required
+def home(request):
+    if request.method == 'POST':
+        form = NeighbourhoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            neighborhood = form.save(commit=False)
+            neighborhood.admin = request.user
+            neighborhood.save()
+            return redirect('hood')
+    else:
+        form = NeighbourhoodForm()
+        neighborhoods = NeighbourHood.objects.all()
+        neighborhoods = neighborhoods[::-1]
+    return render(request, 'index.html', {'form': form, 'neighborhoods': neighborhoods})
